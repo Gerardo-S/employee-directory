cfd1d57a43a2cbfa046ea980d59a0709b9d0a6e2
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-09 15:08:34 -0800

    fixed sort functionality

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -19,13 +19,29 @@ function EmployeeTable(props) {
             fn: (a,b)=> a
         }
     };
+    function compare(a, b) {
+        // Use toUpperCase() to ignore character casing
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+      
+        let comparison = 0;
+        if (nameA > nameB) {
+          comparison = 1;
+        } else if (nameA < nameB) {
+          comparison = -1;
+        }
+        return comparison;
+      }
     const handleSortChange = () => {
+        console.log(currentSort);
         let nextSort;
         if(currentSort === "down"){
             nextSort = "up";
+            characterData.sort(compare);
+            console.log(characterData);
         }
         else if (currentSort === "up"){
-
+            characterData.reverse();
             nextSort= "default"
         }
         else if (currentSort === "default"){
