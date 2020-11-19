af0ad224af292ddd41500c37892174ed23df92c4
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-18 19:07:04 -0800

    modify axios request

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,15 +5,11 @@ import axios from "axios";
 // https://superheroapi.com/api/access-token/character-id/biography
 const BASEURL = "https://www.potterapi.com/v1/characters?key=";
 const APIKEY = "$2a$10$C0VHLQtCHSPD2Yopqj2Nee0FeLO8zp/4KuGZc.Yf./kJFqfB.F1AO";
-const corsFix=  "https://cors-anywhere.herokuapp.com/"
-
-const character =axios.get(corsFix + BASEURL + APIKEY)
+// const character = "characters"
 
 export default {
     getCharacters: function () {
-        console.log(character);
-
-        return character;
+        return axios.get(BASEURL + APIKEY)
     }
 
 };
